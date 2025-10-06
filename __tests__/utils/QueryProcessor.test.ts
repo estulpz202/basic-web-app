96424a5caba13ad92aea043c54ffe35a46193bdb
Estu <estulpzlet@gmail.com>
2025-10-06 13:26:47 -0400

    Added andrewID query and test.

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -18,6 +18,14 @@ describe("QueryProcessor", () => {
           ));
     });
 
+    test('should return Andrew ID', () => {
+        const query = "What is you Andrew ID?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "elopezle"
+          ));
+    });
+
     test('should return name', () => {
         const query = "What is your name?";
         const response: string = QueryProcessor(query);
